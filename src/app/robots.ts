687c47ed2993e81be069f9dccc0f5a364de56ab2
Edwinliby <edwinliby30@gmail.com>
2026-04-29 17:17:24 +0200

    update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: '/privacy-policy',
     },
     sitemap: 'https://edwinliby.dev/sitemap.xml',
   }
