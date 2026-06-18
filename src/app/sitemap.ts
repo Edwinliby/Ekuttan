f2395415452c3b1087445b95692b650a496aa7b0
Edwinliby <edwinliby30@gmail.com>
2026-06-18 10:49:31 +0200

    feat: add sitemap generation for site routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,5 +14,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.2,
     },
+    {
+      url: 'https://edwinliby.dev/app-ads.txt',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.1,
+    },
   ]
 }
