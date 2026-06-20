3f9ac534a94c08dc589c44f3894dfebb7b594739
Edwinliby <edwinliby30@gmail.com>
2026-06-20 10:28:48 +0200

    feat: add robots.txt configuration for search engine indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', '/app-ads.txt'],
       disallow: '/privacy-policy',
     },
     sitemap: 'https://edwinliby.dev/sitemap.xml',
